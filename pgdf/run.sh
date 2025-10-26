b4bdcd894501077830e887b82f1cca206b061ea0
Yujin Lee <bani99@g.skku.edu>
2025-10-27 07:12:56 +0900

    update

--- pgdf/run.sh
+++ pgdf/run.sh
@@ -1,10 +1,25 @@
+python experiments/train_cifar_getPrior_cifar.py --preset c10.rand1
+python experiments/train_cifar_getPrior_cifar.py --preset c10.worse
+python experiments/train_cifar_getPrior_cifar.py --preset c10.aggre
+python experiments/train_cifar_getPrior_cifar.py --preset c10.clean
+
 python experiments/train_cifar_prop1_energyO_mixupX.py --preset c10.worse
 python experiments/train_cifar_prop1_energyO_mixupX.py --preset c10.rand1
 python experiments/train_cifar_prop1_energyO_mixupX.py --preset c10.aggre
 python experiments/train_cifar_prop1_energyO_mixupX.py --preset c10.clean
 
+
+python experiments/train_cifar_getPrior_cifar.py --preset c100.worse
+python experiments/train_cifar_getPrior_cifar.py --preset c100.clean
+
 python experiments/train_cifar_prop1_energyO_mixupX.py --preset c100.worse
 python experiments/train_cifar_prop1_energyO_mixupX.py --preset c100.clean
+
+
+python experiments/train_cifar_getPrior_tinyimagenet.py --preset tinyimagenet.20sym
+python experiments/train_cifar_getPrior_tinyimagenet.py --preset tinyimagenet.50sym
+python experiments/train_cifar_getPrior_tinyimagenet.py --preset tinyimagenet.clean
+python experiments/train_cifar_getPrior_tinyimagenet.py --preset tinyimagenet.40asym
 
 python experiments/train_tinyimagenet_prop1_energy_mixupX.py --preset tinyimagenet.20sym
 python experiments/train_tinyimagenet_prop1_energy_mixupX.py --preset tinyimagenet.50sym
